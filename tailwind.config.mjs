43b0599c754d60894bc988469eeb9db84f3d98d8
Silvarney <silvarney@gmail.com>
2025-08-23 17:16:17 -0300

    Update inicial

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#c2410c',
+        secondary: '#f8f9fa',
+        dark: '#1f2937',
+        light: '#f8f9fa',
+        gray: '#f5f5f5',
+        border: '#e0e0e0'
+      }
+    },
+  },
+  plugins: [],
+}
